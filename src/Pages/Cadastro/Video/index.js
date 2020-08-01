6c360f85d80fc623869859060756a91286dc643e
wheniwant <wheniwantp@gmail.com>
2020-08-01 18:09:33 -0300

    att

---
+++ src/Pages/Cadastro/Video/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageDefault from '../../../components/PageDefault';
+
+function CadastroVideo (){
+    return(
+        <PageDefault>
+            Cadastro de Vídeo
+        </PageDefault>
+    )
+}
+export default CadastroVideo;
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-//import ButtonLink from '../ButtonLink';
 import Button from '../Button';
+import {Link} from 'react-router-dom'
 import LogoOur from '../../assets/img/LogoOur.png';
 import './Menu.css'
 function Menu(){
 
     return (
         <nav className="Menu">
-            <a href='/'>
+            <Link to='/'>
                 <img className='Logo' src={LogoOur} alt='Logo OurFlix'/>
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo Vídeo
             </Button>
             
